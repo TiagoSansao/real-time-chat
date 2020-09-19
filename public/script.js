b3a02f01d67736c90c629b6d26da69418f413b6c
Tiago Sansao <tiagossansao@gmail.com>
2020-09-19 18:10:24 -0300

    Update V.3.0.0

--- public/script.js
+++ public/script.js
@@ -8,11 +8,40 @@ const formName = document.getElementById('form-name');
 const typing = document.getElementById('typing');
 const nameError = document.getElementById('name-error');
 const popup = document.getElementById('popup');
-const messageSubmit  = document.getElementById('message-submit');
 const connectedList = document.getElementById('connected-list');
-var name;
-var count = 0; // Utilized in the ant-flood
+let name;
+let count = 0; // Utilized in the ant-flood
 
+// Functions
+
+function appendUser(username) {
+    const connectedUser = document.createElement('li');
+    connectedUser.innerHTML = username;
+    connectedList.appendChild(connectedUser);
+}
+
+function appendMessage(message, sender) { //if sender is true, he is own who wrote the message
+    const messageElement = document.createElement('span');
+    messageElement.innerHTML = `<i>${new Date().getHours()}:${new Date().getMinutes()}</i> ${message}`;
+    if (sender === true) {
+        messageElement.classList.add('message-owner');
+    }
+    messageElement.classList.add('message');
+    messageContainer.appendChild(messageElement);
+    new Audio("assets/MessageSound.mp3").play();
+}
+function popupOn(msg) {
+    popup.innerHTML = msg;
+    popup.classList.add('popupvisible');
+    setTimeout(() => {popup.classList.remove('popupvisible')}, 1500);
+}
+
+function antFlood() {
+    counst += 1
+    if (count > 0) {
+        setTimeout(() => {count -= 1}, 5000);
+    }
+}
 
 // Receiving and sending information from the server
 
@@ -25,16 +54,16 @@ socket.on('users-list', users => {
     })
 })
 
-socket.on('new-user-message', (name) => {
-    appendMessage(`<b>${name}</b> entrou no chat!`);
+socket.on('new-user-message', (username) => {
+    appendMessage(`<b>${username}</b> entrou no chat!`);
 })
 
-socket.on('disconnect-message', name => {
-    appendMessage(`<b>${name}</b> desconectou-se do chat.`, );
+socket.on('disconnect-message', (username) => {
+    appendMessage(`<b>${username}</b> desconectou-se do chat.`, );
 })
 
 socket.on('reload', () => {
-    location.reload();
+    window.location.reload();
 })
 
 socket.on('user-typing', user => {
@@ -54,16 +83,16 @@ formName.addEventListener('submit', e => { // Login submit
     nameError.innerHTML = '';
     socket.emit('verification', name);
     socket.on('verification', check => {
-        if (check == 0) {
+        if (check === 0) {
             nameError.innerHTML = "Um nome deve ter no minímo 3 caracteres";
-        } else if (check == 1) {
+        } else if (check === 1) {
             nameError.innerHTML = "Este nome já está em uso.";
         } else {
-            let getName = document.getElementById('get-name');
+            const getName = document.getElementById('get-name');
             getName.remove();
             socket.emit('new-user', name);
             document.getElementById('online-people').style.opacity = 1;
-            document.getElementById('rooms').style.opacity = 1;
+            document.getElementById('advertisement').style.opacity = 1;
             appendMessage(`<b>${name}</b> entrou no chat!`, true); 
         }
     })
@@ -71,8 +100,8 @@ formName.addEventListener('submit', e => { // Login submit
 
 sendForm.addEventListener('submit', (e) => { // Send message submit
     e.preventDefault();
-    let message = sendMessage.value;
-    if (message.length != 0) {
+    const message = sendMessage.value;
+    if (message.length !== 0) {
         if (count < 3) {
             socket.emit('send-chat-message', message);
             sendMessage.value = '';
@@ -90,34 +119,3 @@ sendForm.addEventListener('submit', (e) => { // Send message submit
 sendMessage.addEventListener("change", () => { // When someone writes something is triggered
     socket.emit('user-typing', name);
 })
-
-// Functions
-
-function appendUser(name) {
-    let connectedUser = document.createElement('li');
-    connectedUser.innerHTML = name;
-    connectedList.appendChild(connectedUser);
-}
-
-function appendMessage(message, sender) { //if sender is true, he is own who wrote the message
-    let messageElement = document.createElement('span');
-    messageElement.innerHTML = `<i>${new Date().getHours()}:${new Date().getMinutes()}</i> ${message}`;
-    if (sender == true) {
-        messageElement.classList.add('message-owner');
-    }
-    messageElement.classList.add('message');
-    messageContainer.appendChild(messageElement);
-    new Audio("assets/MessageSound.mp3").play();
-}
-function popupOn(msg) {
-    popup.innerHTML = msg;
-    popup.classList.add('popupvisible');
-    setTimeout(() => {popup.classList.remove('popupvisible')}, 1500);
-}
-
-function antFlood() {
-    count++;
-    if (count > 0) {
-        setTimeout(() => {count--}, 5000);
-    }
-}
--- server.js
+++ server.js
@@ -5,89 +5,89 @@ const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server);
 const path = require('path');
+
+// Server port
 const port = 3000;
 
 // Routes
 
-app.use(express.static(path.join(__dirname, "public")));
-app.set('views', path.join(__dirname, "public"));
+app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'public'));
 app.engine('html', require('ejs').renderFile);
 app.set('view-engine', 'html');
 
 app.get('*', (req, res) => {
-    res.render('index.html');
-})
+  res.render('index.html');
+});
 
-
-// Connection with the client
+// Global variables
 
 const users = [];
-var names = []
-
-io.on("connection", socket =>  {
-
-    socket.on('verification', (name) => {
-        
-
-        let nameToCheck = name.replace(/\s+/g, "").toLowerCase()
-
-        if (nameToCheck.length < 3) {
-            socket.emit('verification', 0)
-            return
-        }
-
-        for(user in users) {
-            if (users[user].toLowerCase() == nameToCheck) {
-                socket.emit('verification', 1)
-                return
-            }
-        }
-
-        socket.emit('verification', 3);
-
-    })
-
-    socket.on('new-user', (name) => {
-        users[socket.id] = name;
-        socket.broadcast.emit('new-user-message', name)
-        socket.broadcast.emit('users-list', getConnectedPeople());
-        socket.emit('users-list', names);
-        console.log(`${name} joined the chat!`);
-    })
-
-    socket.on('disconnect', () => {
-        if (users[socket.id] != undefined || users[socket.id] != null) {
-            socket.broadcast.emit('disconnect-message', users[socket.id]);
-        }
-        console.log(`${users[socket.id]} left the chat!`);
-        delete users[socket.id];
-        socket.broadcast.emit('users-list', getConnectedPeople());
-    })
-
-    socket.on('send-chat-message', message =>  {
-        if (users[socket.id] == null) {
-            socket.emit('reload')
-            return
-        }
-        socket.broadcast.emit('chat-message', {message: message, name: users[socket.id]})
-    })
-
-    socket.on('user-typing', user => {
-        socket.broadcast.emit('user-typing', user)
-    })
-
-})
+let names;
 
 // Functions
 
 function getConnectedPeople() {
-    names = [];
-    for (user in users) {
-        names.push(users[user]);
-    }
-    return names;
+  names = Object.values(users);
+  console.log(names)
+  return names;
 }
 
-// Turning on the server 
+// Connection with the client
 
-server.listen(port, () => {console.log(`Server running on port: ${port}`)})
+io.on('connection', (socket) => {
+  socket.on('verification', (name) => {
+    const nameToCheck = name.replace(/\s+/g, '').toLowerCase();
+    if (nameToCheck.length < 3) {
+      socket.emit('verification', 0);
+      return;
+    }
+    if (names !== undefined) {
+      for(let i; i < names.length; i += 1) {
+        if (names[i] === nameToCheck) {
+          socket.emit('verification', 1);
+          return;
+        }
+      }
+    }
+    socket.emit('verification', 3);
+  });
+
+  socket.on('new-user', (name) => {
+    users[socket.id] = name;
+    socket.broadcast.emit('new-user-message', name);
+    socket.broadcast.emit('users-list', getConnectedPeople());
+    socket.emit('users-list', names);
+    console.log(`${name} joined the chat!`);
+  });
+
+  socket.on('disconnect', () => {
+    if (users[socket.id] !== undefined && users[socket.id] !== null) {
+      socket.broadcast.emit('disconnect-message', users[socket.id]);
+    }
+    console.log(`${users[socket.id]} left the chat!`);
+    delete users[socket.id];
+    socket.broadcast.emit('users-list', getConnectedPeople());
+  });
+
+  socket.on('send-chat-message', (message) => {
+    if (users[socket.id] == null) {
+      socket.emit('reload');
+      return;
+    }
+    socket.broadcast.emit('chat-message', {
+      message: message,
+      name: users[socket.id],
+    });
+  });
+
+  socket.on('user-typing', (username) => {
+    socket.broadcast.emit('user-typing', username);
+  });
+});
+
+// Turning on the server
+
+server.listen(port, () => {
+  console.log(`Server running on port: ${port}`);
+});
